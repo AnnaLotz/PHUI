babe1f6977e7f420a369381273e71a59a8a0c178
AnnaLotz <anna.lotz@web.de>
2020-06-05 16:04:18 +0200

    CubeRotator with sliders
    new script to rotate the cube with 3 sliders for each axis

--- PHUI_UnityProject/Assets/Scripts/ObjectController.cs
+++ PHUI_UnityProject/Assets/Scripts/ObjectController.cs
@@ -10,8 +10,9 @@ public class ObjectController : MonoBehaviour
     public GameObject sphere;
     public GameObject cylinder;
 
-    public bool parentIsRotate;
+    public bool parentIsRotate = true;
 
+    //range von 0 bis 90 als speed festlegen
     [Range(0f, 90f)]
     public float speed;
 
@@ -20,20 +21,22 @@ public class ObjectController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Gameobjekt ausschalten bei Start:
-        capsule.SetActive(false);
+        //Gameobjekt ein/ausschalten bei Start:
+        capsule.SetActive(true);
 
         // transformComponent ist standartmäßig drin, alle anderen muss man sich manuell holen
         cubeTransform = cube.transform;
 
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
         //Übergabewerte Objekt und Drehgeschwindigkeit
         //f weil float Wert braucht das halt
-        Rotate(cube, 10f);
+        //Rotate(cube, 10f);
         Rotate(capsule, 20f);
         Rotate(sphere, 30f);
         Rotate(cylinder, 50f);
---
+++ PHUI_UnityProject/Assets/Scripts/Rotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Range(-90f, 90f)]
+    public float gx;
+
+    [Range(-90f, 90f)]
+    public float gy;
+
+    [Range(-90f, 90f)]
+    public float gz;
+
+    //smooth transition to the new position, higher = fast movement
+    float smooth = 20f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //slerp ist für eine smoothe übergangsanimation von aktueller zur taget-rotation
+        Quaternion target = Quaternion.Euler(gx, gy, gz);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        
+    }
+
+}
