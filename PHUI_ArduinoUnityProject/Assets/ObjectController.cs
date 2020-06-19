912fd9dd0504985991302ceeecbba03d070711b2
AnnaLotz <anna.lotz@web.de>
2020-06-19 11:32:38 +0200

    Toggle Objects and uduino working

---
+++ PHUI_ArduinoUnityProject/Assets/ObjectController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    //Objekte Global anlegen, in unity von Hand in den verweis ziehen
+    public GameObject cube;
+    public GameObject capsule;
+    public GameObject sphere;
+    public GameObject cylinder;
+
+    private GameObject[] shapes;
+
+    //bools um shapes ein/aus zu schalten
+    public bool showCube;
+    public bool showCapsule;
+    public bool showSphere;
+    public bool showCylinder;
+
+
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showCube = true;
+        showCapsule = false;
+        showSphere = false;
+        showCylinder = false;
+        ToggleObjects();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToggleObjects();
+
+    }
+    // void Funktionsname(Typ Übergabewert, Typ Übergabewert, ...)
+
+
+    void ToggleObjects()
+    {
+        //Gameobjekt ein/ausschalten bei Start:
+        cube.SetActive(showCube);
+        capsule.SetActive(showCapsule);
+        sphere.SetActive(showSphere);
+        cylinder.SetActive(showCylinder);
+    }
+}
