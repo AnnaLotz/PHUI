8e143641f80101a84246b64dad0442b88cba8ba0
AnnaLotz <anna.lotz@web.de>
2020-06-19 20:58:51 +0200

    Camera, Rotation, VisibiliyToggle
    Rotation und Kamera so angepasst, dass es die Bewegung eines echten Würfels überträgt. Die zur Kamera schauende Seite soll das entsprechende aktive Objekt sein. Objekte wechseln nun simpel ihre sichtbarkeit von 0 auf 1, wenn sie dem gyro nach nach vorne zeigen.

--- PHUI_ArduinoUnityProject/Assets/ObjectController.cs
+++ PHUI_ArduinoUnityProject/Assets/ObjectController.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class ObjectController : MonoBehaviour
@@ -9,45 +13,99 @@ public class ObjectController : MonoBehaviour
     public GameObject capsule;
     public GameObject sphere;
     public GameObject cylinder;
+    public GameObject rect;
+    public GameObject longCylinder;
 
-    private GameObject[] shapes;
+    //Liste, in der alle Totems rein kommen
+    public List<Totem> totemsList = new List<Totem>();
 
-    //bools um shapes ein/aus zu schalten
-    public bool showCube;
-    public bool showCapsule;
-    public bool showSphere;
-    public bool showCylinder;
-
-
-  
+    public int activeSide; //von 1-6
 
 
     // Start is called before the first frame update
     void Start()
     {
-        showCube = true;
-        showCapsule = false;
-        showSphere = false;
-        showCylinder = false;
-        ToggleObjects();
+        //alle gameObjects in die liste schieben
+        totemsList.Add(new Totem(cube, false, 0, 1));
+        totemsList.Add(new Totem(capsule, false, 0, 2));
+        totemsList.Add(new Totem(sphere, false, 0, 3));
+        totemsList.Add(new Totem(cylinder, false, 0, 4));
+        totemsList.Add(new Totem(rect, false, 0, 5));
+        totemsList.Add(new Totem(longCylinder, false, 0, 6));
 
+        //durch die liste aller Totems iterieren
+        foreach (Totem gameobj in totemsList) 
+        {
+            Debug.Log(gameobj);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindActiveSide();
         ToggleObjects();
-
     }
-    // void Funktionsname(Typ Übergabewert, Typ Übergabewert, ...)
+   
 
+    public void FindActiveSide() {
+        //Berechne winkel zwischen lokalem Vektor und globalen up-Vektor
+        float angle1 = Vector3.Angle(transform.up, Vector3.right);
+        float angle2 = Vector3.Angle(transform.right, Vector3.right);
+        float angle3 = Vector3.Angle(transform.forward, Vector3.right);
+        float angle4 = Vector3.Angle(- transform.up, Vector3.right);
+        float angle5 = Vector3.Angle(- transform.right, Vector3.right);
+        float angle6 = Vector3.Angle(- transform.forward, Vector3.right);
+        float[] angles = { angle1, angle2, angle3, angle4, angle5, angle6 };
+        
+        //wenn der winkel zwischen -45 und 45 liegt, ist das die front-facing-side
+        for (int i = 0; i < angles.Length; i++)
+        {           
+            if (angles[i] < 45 && angles[i] > -45)
+            {
+                activeSide = i + 1;                
+            }
+        }
+        Debug.Log(activeSide + " is ActiveSide");
+    }
 
     void ToggleObjects()
     {
-        //Gameobjekt ein/ausschalten bei Start:
-        cube.SetActive(showCube);
-        capsule.SetActive(showCapsule);
-        sphere.SetActive(showSphere);
-        cylinder.SetActive(showCylinder);
+        //Gameobjekt ein/ausschalten:
+        foreach (Totem gameobj in totemsList)
+        {
+            gameobj.isActiveBool = false;
+            if (gameobj.cubeSide == activeSide)
+                gameobj.isActiveBool = true;
+            gameobj.ToggleVisibility();
+        }
     }
+
+
+
+    //**********************************************************************************************
+    public class Totem 
+    {
+        public GameObject gameObject;
+        public bool isActiveBool;
+        public float visibilityPercentage;
+        public int cubeSide;
+
+        //constructor in Unity:
+        public Totem(GameObject _gameObject, bool _isActiveBool, float _visibilityPercentage, int _cubeSide)
+        {
+            gameObject = _gameObject;
+            isActiveBool = _isActiveBool;
+            visibilityPercentage = _visibilityPercentage;
+            cubeSide = _cubeSide;
+        }
+
+        public void ToggleVisibility() 
+        {
+            gameObject.SetActive(isActiveBool);
+        }
+    }
+    
+
+
 }
--- PHUI_ArduinoUnityProject/Assets/Projects/IMU/ReceiveIMUValues.cs
+++ PHUI_ArduinoUnityProject/Assets/Projects/IMU/ReceiveIMUValues.cs
@@ -5,7 +5,7 @@ public class ReceiveIMUValues : MonoBehaviour {
 
     Vector3 position;
     Vector3 rotation;
-    public Vector3 rotationOffset ;
+    public Vector3 rotationOffset;
     public float speedFactor = 15.0f;
     public string imuName = "r"; // You should ignore this if there is one IMU.
 
@@ -25,12 +25,12 @@ public class ReceiveIMUValues : MonoBehaviour {
             float x = float.Parse(values[2]);
             float y = float.Parse(values[3]);
             float z = float.Parse(values[4]);
-            this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation,  new Quaternion(w, y, x, z), Time.deltaTime * speedFactor);
+            this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation,  new Quaternion(x, y, z, w), Time.deltaTime * speedFactor);
         } else if (values.Length != 5)
         {
             Debug.LogWarning(data);
         }
-        this.transform.parent.transform.eulerAngles = rotationOffset;
+        //this.transform.parent.transform.eulerAngles = rotationOffset;
       //  Log.Debug("The new rotation is : " + transform.Find("IMU_Object").eulerAngles);
     }
 }
